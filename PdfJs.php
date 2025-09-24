8183eba64eca5690e9e982cefcc137a8897c143a
Konstantin Krivosheev <krivosheev.artint@gmail.com>
2025-09-24 09:24:26 +0300

    add bookmarks

--- PdfJs.php
+++ PdfJs.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace yii2assets\pdfjs;
+
 use Yii;
 use yii\helpers\Html;
 use yii\helpers\Url;
@@ -12,7 +13,7 @@ use yii\helpers\ArrayHelper;
  */
 class PdfJs extends \yii\base\Widget
 {
-    public $url=null;
+    public $url = null;
 
     public $width = '100%';
 
@@ -21,25 +22,36 @@ class PdfJs extends \yii\base\Widget
     public $options = [];
 
     public $buttons = [];
+    public $bookmarks = [];
 
     public function init()
     {
         parent::init();
         $buttons = Yii::$app->getModule('pdfjs')->buttons;
-        $this->buttons = array_merge($buttons,$this->buttons);
+        $this->buttons = array_merge($buttons, $this->buttons);
+        // Настройки по умолчанию для закладок
+        $this->bookmarks = array_merge([
+            'enabled' => true,
+            'storage' => 'local', // local или session
+            'position' => 'left', // left, right, top
+            'autoSave' => true,
+            'showPanel' => true,
+            'panelWidth' => '250px'
+        ], $this->bookmarks);
         //$this->getView()->registerJsFile(Yii::$app->assetManager->getPublishedUrl('@yii2assets/pdfjs/assets').'/yii2-pdfjs.js');
     }
 
     public function run()
     {
-      if(!array_key_exists('style',$this->options)){
-        $this->options['style'] = 'border:solid 2px #404040; width:'.$this->width.'; height:'.$this->height.';';
-      }
-      return $this->render('viewer',[
-        'options' => $this->options,
-        'url' => $this->url,
-        'buttons'=>$this->buttons,
-        'id'=>$this->id
-      ]);
+        if (!array_key_exists('style', $this->options)) {
+            $this->options['style'] = 'border:solid 2px #404040; width:' . $this->width . '; height:' . $this->height . ';';
+        }
+        return $this->render('viewer', [
+            'options' => $this->options,
+            'url' => $this->url,
+            'buttons' => $this->buttons,
+            'id' => $this->id,
+            'bookmarks' => $this->bookmarks
+        ]);
     }
 }
